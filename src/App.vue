49be5186c8ac755cad3a3845512c3f11bca307e7
Max <scorpiomax90@gmail.com>
2023-01-16 23:39:44 +0200

    transform css app

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="wrapper">
+		<header class="header">
+			<h1 class="header__title">
+				Transform CSS Generator
+			</h1>
+		</header>
+
+		<main class="main">
+			<transform-generator />
+		</main>
+
+		<footer class="footer">
+			<span>© 2023</span> | <a class="footer__link" href="https://github.com/" target="_blank">
+				<icon-github />
+				<span>GitHub</span>
+			</a>
+		</footer>
+	</div>
+</template>
+
+<script>
+import TransformGenerator from './components/TransformGenerator.vue'
+import IconGithub from './components/icons/IconGithub.vue'
+
+export default {
+  components: { 
+		TransformGenerator,
+		IconGithub
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TransformGenerator.vue
@@ -0,0 +1,153 @@
+<template>
+	<section class="generator">
+		<div class="generator__container">
+			<div class="generator__table">
+				<input type="text" v-model="textInput">
+
+				<label class="generator__label">Text color: {{ textColor }}</label>
+				<input type="color" v-model="textColor">
+
+				<label class="generator__label">Background: {{ bgColor }}</label>
+				<input type="color" v-model="bgColor">
+
+				<label class="generator__label">Rotate: {{ rotate }}deg;</label>
+				<input type="range" min="-180" max="180" v-model="rotate">
+
+				<label class="generator__label">Scale: {{ scale }};</label>
+				<input type="range" min="0.1" max="2" step="0.1" v-model="scale">
+
+				<label class="generator__label">Translate X: {{ translateX }}px;</label>
+				<input type="range" min="-100" max="100" v-model="translateX">
+
+				<label class="generator__label">Translate Y: {{ translateY }}px;</label>
+				<input type="range" min="-100" max="100" v-model="translateY">
+
+				<label class="generator__label">Skew X: {{ skewX }}deg;</label>
+				<input type="range" min="-90" max="90" v-model="skewX">
+
+				<label class="generator__label">Skew Y: {{ skewY }}deg;</label>
+				<input type="range" min="-90" max="90" v-model="skewY">
+
+				<div class="generator__buttons">
+					<button class="btn btn-main" @click.prevent="reset">Reset</button>
+					<button class="btn btn-success">Copy</button>
+				</div>
+			</div>
+			<div class="generator__result">
+				<div class="generator__output" :style="output">
+					{{ textInput }}
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			textColor: '#ffffff',
+			bgColor: '#3344c1',
+			rotate: 0,
+			scale: 1,
+			translateX: 0,
+			translateY: 0,
+			skewX: 0,
+			skewY: 0,
+			textInput: 'Your text!'
+		}
+	},
+	computed: {
+		output() {
+			return {
+				transform: `
+					rotate(${this.rotate}deg)
+					scale(${this.scale})
+					translateX(${this.translateX}px)
+					translateY(${this.translateY}px)
+					skewX(${this.skewX}deg)
+					skewY(${this.skewY}deg)
+				`,
+				color: `${this.textColor}`,
+				backgroundColor: `${this.bgColor}`
+			}
+		}
+	},
+	methods: {
+		reset() {
+			this.textColor = '#ffffff',
+			this.bgColor = '#3344c1',
+			this.rotate = 0,
+			this.scale = 1,
+			this.translateX = 0,
+			this.translateY = 0,
+			this.skewX = 0,
+			this.skewY = 0,
+			this.textInput = 'Your text!'
+		}
+	}
+}
+</script>
+
+<style>
+input[type="text"] {
+	font-family: Montserrat;
+	margin-bottom: .5rem;
+	font-size: 1rem;
+	padding: 5px 8px 4px;
+	border: none;
+	border-radius: 6px;
+	box-shadow: 0 0 0 1px var(--bg-main-color);
+}
+input[type="text"]:focus {
+	outline: none;
+	box-shadow: 0 0 0 2px var(--blue-color-hover);
+}
+input[type="color"] {
+	height: 34px;
+	border-radius: 6px;
+}
+
+.generator__label + input {
+	margin-bottom: .5rem;
+}
+.generator {
+	padding: 2.5rem 15px;
+}
+.generator__container {
+	max-width: 800px;
+	margin: 0 auto;
+	padding: 15px;
+	border-radius: 6px;
+	background: var(--bg-second-color);
+	display: flex;
+	column-gap: 15px;
+}
+.generator__table {
+	flex: 1 0 45%;
+	color: var(--black-color);
+}
+
+.generator__result {
+	position: relative;
+	flex: 1 0 45%;
+	overflow: hidden;
+	border-radius: 6px;
+	background: var(--white-color);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.generator__output {
+	padding: 0.8rem 1.5rem;
+	font-weight: 600;
+	border-radius: 6px;
+}
+
+.generator__buttons {
+	margin: 20px 0 0;
+	display: flex;
+	gap: 15px;
+	justify-content: flex-end;
+}
+</style>
